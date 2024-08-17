ade2da8d223d90a94f3510d4d44a18f4455f644a
runinbk <runinbk@gmail.com>
2024-08-17 13:05:24 -0400

    Agregar archivos de proyecto.

---
+++ OpenTK-T/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using OpenTK_T;
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (Game game = new Game(800, 600, "LearnOpenTK"))
+        {
+            game.Run();
+        }
+    }
+}
